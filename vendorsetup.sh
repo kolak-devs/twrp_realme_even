de85ae29a98326bb45e70d4ebf93437810177251
kolak-devs <rulsrender2@gmail.com>
2026-07-20 20:34:24 +0700

    even: add vendorsetup
    Signed-off-by: kolak-devs <rulsrender2@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+export ALLOW_MISSING_DEPENDENCIES="true"
+export FOX_USE_NANO_EDITOR="1"
+export FOX_DELETE_AROMAFM="1"
+export OF_MAINTAINER="ismasrull"
+export FOX_BUILD_TYPE="stable"
+export FOX_VERSION="R12.1"

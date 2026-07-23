8e0bbbf68bdd3cce134eafd5fcfc08fe0173f2c1
kolak-devs <rulsrender2@gmail.com>
2026-07-23 20:07:59 +0700

    pbrp:

--- vendorsetup.sh
+++
@@ -1,8 +0,0 @@
-export ALLOW_MISSING_DEPENDENCIES="true"
-export FOX_USE_NANO_EDITOR="1"
-export FOX_DELETE_AROMAFM="1"
-export OF_MAINTAINER="ismasrull"
-export FOX_BUILD_TYPE="stable"
-export OF_FL_PATH1="/proc/qcom_flash" 
-export OF_FL_PATH2=""
-
